cc650744dc94e60489db2d059f78297ba8d1223b
kich555 <hyunjjang199582@gmail.com>
2022-10-03 21:46:29 +0900

    Create tester api request for error handling test

--- boiler-plate/src/apis/index.ts
+++ boiler-plate/src/apis/index.ts
@@ -1,6 +1,8 @@
 import { AxiosPromise } from 'axios';
 import request from './request';
-interface ResponseData<T> {
+// import request from 'apis/test';
+
+export interface ResponseData<T = any> {
   code: number;
   data: T;
   message: string;
@@ -8,13 +10,6 @@ interface ResponseData<T> {
 }
 
 //for example
-interface UserInfo {
-  username?: '';
-}
-interface TestProps {
-  userId?: string;
-}
-
 interface TodoData {
   completed: boolean;
   id: number;
@@ -30,8 +25,32 @@ AxiosResponse -> axios에서 제공하는 기본적인 response 구조체
 ResponseData -> back-end developer에 의해 wrapping된 api response 구조체
 SpecificData ->  UI or 실질적인 business logic에 영향을 끼치게 될 real data
 */
-export const getWithoutParamsTestRequest = (): AxiosPromise<ResponseData<TodoData>> => request('get', 'https://jsonplaceholder.typicode.com/todos/1');
+export const getTestRequestWithoutParams = async (): Promise<ResponseData<TodoData>> =>
+  // throw new Error('9');
+  // throw { type: 'unknown', test: 'this work?' };
+  // throw 'test';
+  request('get', `https://jsonplaceholder.typicode.com/todos/1`);
 
-// export const testRequest = (params: TestProps = {}): AxiosPromise<ResponseData<UserInfo>> => request('post', 'https://api.themoviedb.org/3/movie/550?api_key=edd2f4791601915cc3444664865aae15', params);
+export const getTestRequestWithParams = async (id: string): Promise<ResponseData<TodoData>> => request('get', `https://jsonplaceholder.typicode.com/todos/${id}`);
 
-// 'https://api.themoviedb.org/3/movie/popular?api_key=edd2f4791601915cc3444664865aae15'
+// export const getWithoutParamsTestRequest: REQ<TodoRequestProps, TodoData> = id =>
+//   request(
+//     'get',
+//     `https://jsonplaceholder.typicode.com/todos/${id}`,
+//     {},
+//     {
+//       headers: {
+//         'content-type': 'application/json',
+//       },
+//     },
+//     {
+//       request: (request: any) => {
+//         // 处理请求
+//         console.log(request);
+//       },
+//       response: (response: any) => {
+//         // 处理响应
+//         console.log(response);
+//       },
+//     },
+//   );
