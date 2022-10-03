66ddf5d696dc4ef94c6b56b01fe3eac31e1da0c7
kich555 <hyunjjang199582@gmail.com>
2022-10-04 05:26:02 +0900

    Testing apis

--- boiler-plate/src/apis/index.ts
+++ boiler-plate/src/apis/index.ts
@@ -1,7 +1,6 @@
-import { AxiosPromise } from 'axios';
 import request from './request';
 // import request from 'apis/test';
-
+import type { AxiosRequestConfig } from 'axios';
 export interface ResponseData<T = any> {
   code: number;
   data: T;
@@ -25,32 +24,23 @@ AxiosResponse -> axios에서 제공하는 기본적인 response 구조체
 ResponseData -> back-end developer에 의해 wrapping된 api response 구조체
 SpecificData ->  UI or 실질적인 business logic에 영향을 끼치게 될 real data
 */
-export const getTestRequestWithoutParams = async (): Promise<ResponseData<TodoData>> =>
+export const getTestRequestWithoutParams = async (): Promise<ResponseData<TodoData>> => {
+  console.log('for prevent format on save');
   // throw new Error('9');
-  // throw { type: 'unknown', test: 'this work?' };
+  // throw { userId: 1, id: 1, title: 'delectus aut autem', completed: false };
   // throw 'test';
-  request('get', `https://jsonplaceholder.typicode.com/todos/1`);
+  return request({ url: `https://jsonplaceholder.typicode.com/todos/1` });
+};
 
-export const getTestRequestWithParams = async (id: string): Promise<ResponseData<TodoData>> => request('get', `https://jsonplaceholder.typicode.com/todos/${id}`);
-
-// export const getWithoutParamsTestRequest: REQ<TodoRequestProps, TodoData> = id =>
-//   request(
-//     'get',
-//     `https://jsonplaceholder.typicode.com/todos/${id}`,
-//     {},
-//     {
-//       headers: {
-//         'content-type': 'application/json',
-//       },
-//     },
-//     {
-//       request: (request: any) => {
-//         // 处理请求
-//         console.log(request);
-//       },
-//       response: (response: any) => {
-//         // 处理响应
-//         console.log(response);
-//       },
-//     },
-//   );
+export const getTestRequestWithParams = async (todoId: string): Promise<ResponseData<TodoData>> =>
+  request({
+    url: `https://jsonplaceholder.typicode.com/todos/${todoId}`,
+    specificInterceptor: {
+      request: config => {
+        console.log('for prevent format on save');
+        // 요청을 보내기 전에 수행할 로직
+        return config;
+      },
+      response: response => response,
+    },
+  });
